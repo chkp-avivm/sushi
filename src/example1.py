0b32c46255cb1c4fdde601fe9971d78ce9d43cf2
Devloop Agent <devloop-ai@checkpoint.com>
2025-07-23 20:32:45 +0000

    Fix pipeline

--- src/example1.py
+++ src/example1.py
@@ -1,5 +1,6 @@
 
-
 def greet():
-    print("Very very very very Very very very very Very very very very Very very very very long sentence")
+    print(
+        "Very very very very Very very very very Very very very very Very very very very long sentence"
+    )
     print("Hello, world!")
