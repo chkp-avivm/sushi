de083bcd43888926c94f49182b3d8e3a9fd9ac2b
Aviv Meydan <avivm@checkpoint.com>
2025-07-23 23:38:37 +0300

    Update example1.py

--- src/example1.py
+++ src/example1.py
@@ -1,5 +1,4 @@
 # example1.py with indentation and whitespace errors
 
 def greet( ):
-    print("Very very very very Very very very very Very very very very Very very very very long sentence" )
-    print( "Hello, world!")  
+    print( "Hello, world!" )  
