79857e8edfb91cd1eb103c964ba9b0413c60f69a
Aviv Meydan <avivm@checkpoint.com>
2025-07-23 23:20:52 +0300

    Create test_file.py

---
+++ src/test_file.py
@@ -0,0 +1,5 @@
+# example1.py with indentation and whitespace errors
+
+def greet( ):
+    print("Very very very very Very very very very Very very very very Very very very very long sentence" )
+    print( "Hello, world!")  
